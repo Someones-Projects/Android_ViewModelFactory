75230eaee9ff8b51dd42fe9718b65cfd41de2ad4
Fahri Can <fahri-can@gmx.at>
2020-09-13 20:23:03 +0200

    Create ViewModelKey.kt

---
+++ ViewModelKey.kt
@@ -0,0 +1,14 @@
+package com.onecosys.android.bitcoinnews.di.factory
+
+import androidx.lifecycle.AndroidViewModel
+import dagger.MapKey
+import kotlin.reflect.KClass
+
+@Target(
+    AnnotationTarget.FUNCTION,
+    AnnotationTarget.PROPERTY_GETTER,
+    AnnotationTarget.PROPERTY_SETTER
+)
+@Retention(AnnotationRetention.RUNTIME)
+@MapKey
+annotation class ViewModelKey(val value: KClass<out AndroidViewModel>)
